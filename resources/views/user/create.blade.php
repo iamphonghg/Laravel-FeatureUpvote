dc7c4a69d6329fd2d7452bf2b281d6a598a7cb65
Phong Hoang Gia <gioxoay.xp@gmail.com>
2021-07-29 11:04:39 +0700

    fix bugs in create view

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -11,28 +11,18 @@
         <div class="container">
             <div class="row my-5">
                 <div class="col-lg-12">
-                    @php
-                        $title = $content = '';
-                        if ($isEdited) {
-                            $title = $suggestion->title;
-                            $content = $suggestion->content;
-                        }
-                    @endphp
                     <form action="{{ route('suggestions.store', [$board->short_name]) }}" method="GET">
                         @csrf
                         <label for="title" class="form-label"><span class="fw-bold h5">Title</span></label>
                         <div class="mb-4 input-group">
-                            <input type="text" class="form-control" id="title" placeholder="Your suggestion" name="title" value="{{ $title }}">
+                            <input type="text" class="form-control" id="title" placeholder="Your suggestion" name="title" >
                         </div>
 
                         <label for="description" class="form-label"><span class="fw-bold h5">Description</span></label>
                         <div class="mb-4 input-group">
-                            <textarea id="query" class="form-control" style="height: 140px" placeholder="Description of your suggestion (optional)" name="content">{{ $content }}</textarea>
+                            <textarea id="query" class="form-control" style="height: 140px" placeholder="Description of your suggestion (optional)" name="content"></textarea>
                         </div>
-
-
-                            <a href="" class="btn btn-outline-secondary mb-4">Add image (optional)</a>
-
+                        <a href="" class="btn btn-outline-secondary mb-4">Add image (optional)</a>
                         @php
                             $name = $email = $shop_name = '';
                             $lockInfo = '';
@@ -75,18 +65,10 @@
 
                         <div class="d-flex mb-4 text-center justify-content-end">
                             <button type="submit" class="btn btn-secondary me-2">
-                                @if (!$isEdited)
-                                    Post suggestion
-                                @else
-                                    Save
-                                @endif
+                                Post suggestion
                             </button>
                             <a href="/" class="btn btn-outline-secondary">Cancel</a>
                         </div>
-                        @if ($isEdited)
-                            <input type="hidden" name="isEdited" value="{{ true }}">
-                            <input type="hidden" name="suggestion_id" value="{{ $suggestion->id }}">
-                        @endif
                     </form>
                 </div>
             </div>
